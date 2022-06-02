31d5729775f925fc79928035d901c4de1c74aa7c
root <kenneth4justice@gmail.com>
2022-06-02 14:25:33 -0700

    Create a dynamic script with argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     if len(argv) < 2:
         print("0 arguments.")
     elif len(argv) == 2:
-        print("1 argument:\n1: {}".format(argv[1]))
+        print("1 argument:\n: {}".format(argv[1]))
     else:
         print("{} arguments:".format(len(args) - 1))
         for arg in range(1, len(argv)):
