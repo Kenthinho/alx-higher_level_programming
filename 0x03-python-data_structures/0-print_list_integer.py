c31464056a7126850a785e0b4acb9425f0dabc19
root <kenneth4justice@gmail.com>
2022-06-05 12:32:15 -0700

    This function prints each integer value on a newline

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integers(my_list=[]):
     '''This prints each number in the list on a new line'''
     for number in range(len(my_list)):
-        print("{}".format(my_list[number]))
+        print("{:d}".format(my_list[number]))
