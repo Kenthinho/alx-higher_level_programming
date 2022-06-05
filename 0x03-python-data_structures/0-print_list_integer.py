fbe09d0b966623ef7067c25e3b1fe648b54b82a7
root <kenneth4justice@gmail.com>
2022-06-05 12:34:49 -0700

    prints all integers in the list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integers(my_list=[]):
-    '''This prints each number in the list on a new line'''
     for number in range(len(my_list)):
-        print("{:d}".format(my_list[number]))
+        print("{:d}".format(int(my_list[number])))
