5b7f80a8efb00b875234f8ca0050de5a6d91aad4
root <kenneth4justice@gmail.com>
2022-06-20 14:40:06 -0700

    Prints specified number of elements

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     no_of_elements = 0
     try:
@@ -6,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(value), end='')
             no_of_elements += 1
         print()
-    except:
-        pass
+    except (ValueError, IndexError, TypeError):
+        break
     return no_of_elements
