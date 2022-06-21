043a6d5c059f878dbc5056847597b54f6d6ef491
root <kenneth4justice@gmail.com>
2022-06-20 18:15:09 -0700

    Integer division files

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
+    result = None
     try:
         result = a / b
     except (ValueError, TypeError, ZeroDivisionError):
-        result = None
+        pass
     finally:
         print("inside result: {}".format(result))
     return result
