0aef4e9c71cb9068535b093237784dc64fcb8a8b
root <kenneth4justice@gmail.com>
2022-06-21 07:40:00 -0700

    Create a new square object

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Defines a square class'''
+
+
+class Square:
+    '''This represents object made of square'''
+
+
+    def __init__(self, size):
+        '''Initializes a new square object
+        Args:
+            size (int): The size of the square object
+        '''
+        self.__size = size
