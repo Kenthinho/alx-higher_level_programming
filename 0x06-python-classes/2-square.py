480315fee402b08172d7234c90c4b568a1e90116
root <kenneth4justice@gmail.com>
2022-06-21 07:52:01 -0700

    Add validations to square objects

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Defines a square class'''
+
+
+class Square:
+    '''This represents object made of square'''
+
+    def __init__(self, size=0):
+        '''Initializes a new square object
+        Args:
+            size (int): The size of the square object
+        '''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
