ae95ff9b2c791d8c1df8c3af44acdf4cedf20778
root <kenneth4justice@gmail.com>
2022-06-21 02:34:27 -0700

    prints integer division safely

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,7 +5,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except (ValueError, TypeError, ZeroDivisionError):
-        pass
+        result = None
     finally:
         print("inside result: {}".format(result))
     return result
