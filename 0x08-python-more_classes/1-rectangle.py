bc5c6a91a0e3a9422b576743ecf8a87292d02dac
root <kenneth4justice@gmail.com>
2022-06-27 05:04:09 -0700

    create private instance attributes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,28 +17,6 @@ class Rectangle:
         self.height = height
 
     @property
-    def width(self):
-        '''Retrieves the value of the width
-
-        Returns: an int value
-        '''
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        '''modifies the value of the width
-
-        Args (int):
-            value - argument
-        '''
-        if not isinstance(value, int):
-            raise TupeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
-
-    @property
     def height(self):
         '''retrieves the value of the height
 
@@ -59,3 +37,24 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
+
+    @property
+    def width(self):
+        '''Retrieves the value of the width
+
+        Return: an int value
+        '''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''modifies the value of the width
+        Args (int):
+            value - argument
+        '''
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
