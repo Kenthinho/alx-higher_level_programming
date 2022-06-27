413ae483eb4ac7943a9f5c93d0770337101005d6
root <kenneth4justice@gmail.com>
2022-06-27 05:58:57 -0700

    Add class variable to count number of instances made

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 '''A module made for working with a rectangle
 '''
-number_of_instances = 0
 
 
 class Rectangle:
     """Represents a 2D Polygon with 4 perpendicular sides
     """
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         '''Initializes each instance with its parameters
 
