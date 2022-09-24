12bf7c20688527b34098b54a16148316d4853e99
root <kenneth4justice@gmail.com>
2022-09-23 17:31:10 -0700

    Python Networking scripts

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Sends a form data to a URL."""
+import sys
+import requests
+
+
+if __name__ == '__main__' and if len(sys.argv) > 2:
+    url = sys.argv[1]
+    email = sys.argv[2]
+    form_data = [('email', email)]
+    response = requests.post(url, data=form_data)
+    print(response.text)
---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+import requests
+
+
+if __name__ == '__main__' and if len(sys.argv) > 1:
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print('Error code: {}'.format(response.status_code))
+    else:
+        print(response.text)
